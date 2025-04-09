8f14077e5234f00be91d6a07557bdbc7f6bcba39
shinsouhitoshi1203 <24521353@gm.uit.edu.vn>
2025-04-09 11:26:28 +0700

    reset the default duration

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -8,9 +8,9 @@ const useStore = create(
 				inStage: "focus",
 				duration: {
 					// in minutes
-					focus: 3 / 60,
-					short: 1 / 60,
-					long: 2 / 60
+					focus: 25,
+					short: 5,
+					long: 15
 				},
 				viewTimeRemaining: true
 			},
